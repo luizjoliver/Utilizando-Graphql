c70939df94f546551a6622678afaf5b393084649
Luiz fellipe Joliver Gonçalves e silva <106269210+luizjoliver@users.noreply.github.com>
2023-12-06 13:45:36 -0300

    First time using graphql with apollo client (using schema first approach)

---
+++ first/simple-server.ts
@@ -0,0 +1,55 @@
+import {ApolloServer, gql} from "apollo-server"
+import {randomUUID} from "node:crypto"
+
+//Graphql Schema first Approach 
+
+type User = {
+    id:string,
+    name:string
+}
+
+const users :User[] = []
+
+const typeDefs = gql`
+
+type User  {
+    id:String!,
+    name:String!
+}
+type Query {
+    users: [User!]!
+}
+
+type Mutation {
+    createUser(name:String!) : User!
+
+}
+
+`
+
+const server = new ApolloServer({typeDefs,
+resolvers:{
+    Query:{
+        users: () =>{
+            return users
+        }
+    },
+    Mutation:{
+        createUser:(parent_,args,ctx_) =>{
+            const user = {
+                id:randomUUID(),
+                name:args.name
+            }
+            users.push(user)
+
+            return user
+            
+
+        }
+    }
+}})
+
+server.listen().then(({url})=>{
+    console.log(`HTTP server running on ${url}`);
+    
+})
